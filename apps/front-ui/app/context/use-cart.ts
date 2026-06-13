c8a73fb4fe78d5d41fb32dc9081bb593778edf17
EzechielKiregha <ekiregha@gmail.com>
2026-06-13 19:50:40 +0200

    cart item

--- apps/front-ui/app/context/use-cart.ts
+++ apps/front-ui/app/context/use-cart.ts
@@ -5,7 +5,7 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 export interface CartItem {
-  product: ProductEntity
+  product: any;
   quantity: number;
 }
 
