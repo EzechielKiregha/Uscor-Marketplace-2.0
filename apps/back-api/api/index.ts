edbed9377eed1a1c4354f8eb7f6bec67c3327128
EzechielKiregha <ekiregha@gmail.com>
2026-07-10 00:53:58 +0200

    new vercel domain added

--- apps/back-api/api/index.ts
+++ apps/back-api/api/index.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
 		app.enableCors({
 			origin: [
 				"http://localhost:3000",
+				"https://uscor.vercel.app",
 				"https://uscor-marketplace-2-0-front-ui.vercel.app",
 			],
 			credentials: true,
